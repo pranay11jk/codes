59b3e95828b59eefe0e2648c813413eda5992f2c
pranay11jk <138625433+pranay11jk@users.noreply.github.com>
2025-04-19 00:54:08 +0530

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,11 @@
+# Simple addition of two numbers in Python
+
+# Input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Addition
+sum = num1 + num2
+
+# Output
+print("The sum is:", sum)
